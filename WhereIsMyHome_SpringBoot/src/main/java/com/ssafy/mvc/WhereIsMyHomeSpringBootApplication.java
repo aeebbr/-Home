ae2b68fd5eeb5bf12f173b0ef699212bdf9072db
임경민 <aeebbr@gmail.com>
2022-11-02 14:32:10 +0900

    basePackages 설정

--- WhereIsMyHome_SpringBoot/src/main/java/com/ssafy/mvc/WhereIsMyHomeSpringBootApplication.java
+++ WhereIsMyHome_SpringBoot/src/main/java/com/ssafy/mvc/WhereIsMyHomeSpringBootApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.ssafy"})
 public class WhereIsMyHomeSpringBootApplication {
 
 	public static void main(String[] args) {

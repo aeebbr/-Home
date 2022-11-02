7cc46d5e91706c2c491a8fc8e76c68bd054d72ee
임경민 <aeebbr@gmail.com>
2022-11-02 14:24:08 +0900

    addr controller, serviceImpl 수정

--- WhereIsMyHome_SpringBoot/src/main/java/com/ssafy/addr/service/AddressServiceImpl.java
+++ WhereIsMyHome_SpringBoot/src/main/java/com/ssafy/addr/service/AddressServiceImpl.java
@@ -2,37 +2,31 @@ package com.ssafy.addr.service;
 
 import java.util.List;
 
-import com.ssafy.addr.dao.AddressDaoImpl;
+import org.springframework.stereotype.Service;
+
 import com.ssafy.addr.dto.AddressDto;
+import com.ssafy.addr.dto.mapper.AddressMapper;
 
+@Service
 public class AddressServiceImpl implements IAddressService {
-
-	private static AddressServiceImpl instance;
-
-	private AddressServiceImpl() {
+	private AddressMapper addressMapper;
+	
+	private AddressServiceImpl(AddressMapper addressMapper) {
+		this.addressMapper = addressMapper;
 	}
 
-	public static AddressServiceImpl getInstance() {
-		if (instance == null) {
-			instance = new AddressServiceImpl();
-		}
-		return instance;
-	}
-	
-	private AddressDaoImpl addrDao = AddressDaoImpl.getInstance();
-	
 	@Override
 	public List<AddressDto> getSidoList() {
-		return addrDao.getSidoList();
+		return addressMapper.getSidoList();
 	}
 
 	@Override
 	public List<AddressDto> getGugunList(String sidoCode) {
-		return addrDao.getGugunList(sidoCode);
+		return addressMapper.getGugunList(sidoCode);
 	}
 
 	@Override
 	public List<AddressDto> getDongList(String gugunCode) {
-		return addrDao.getDongList(gugunCode);
+		return addressMapper.getDongList(gugunCode);
 	}
 }

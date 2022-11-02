adac82456f45afeac4e32abae9d32657fe4cd616
aeebbr <aeebbr@gmail.com>
2022-11-03 02:06:36 +0900

    작업물 임시 저장

--- WhereIsMyHome_SpringBoot/src/main/java/com/ssafy/addr/controller/AddressController.java
+++ WhereIsMyHome_SpringBoot/src/main/java/com/ssafy/addr/controller/AddressController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ssafy.addr.dto.AddressDto;
@@ -55,6 +56,8 @@ public class AddressController {
 		addrList = addrService.getSidoList();
 
 		JSONObject json = buildJsonObj(addrList);
+		
+		System.out.println(json.toJSONString());
 		try {
 			res.getWriter().write(json.toJSONString());
 		} catch (IOException e) {
